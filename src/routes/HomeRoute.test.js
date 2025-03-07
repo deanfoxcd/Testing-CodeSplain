8bef9e99e19cc217dea369a1d1430c42a2ec0c2e
Dean Fox <deanfoxcd@gmail.com>
2025-03-06 22:22:14 -0500

    add reusable create server function

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -3,36 +3,68 @@ import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import HomeRoute from './HomeRoute';
 import { MemoryRouter } from 'react-router';
+import { createServer } from '../test/server';
 
-const handlers = [
-  rest.get('/api/repositories', (req, res, ctx) => {
-    const language = req.url.searchParams.get('q').split('language:')[1];
-    console.log(language);
+// const handlers = [
+//   rest.get('/api/repositories', (req, res, ctx) => {
+//     const language = req.url.searchParams.get('q').split('language:')[1];
+//     console.log(language);
 
-    return res(
-      ctx.json({
+//     return res(
+//       ctx.json({
+//         items: [
+//           { id: 1, full_name: `${language}_one` },
+//           { id: 2, full_name: `${language}_two` },
+//         ],
+//       })
+//     );
+//   }),
+// ];
+
+// const server = setupServer(...handlers);
+
+// beforeAll(() => {
+//   server.listen();
+// });
+
+// afterEach(() => {
+//   server.resetHandlers();
+// });
+
+// afterAll(() => {
+//   server.close();
+// });
+
+// createServer([
+//   {
+//     path: '/api/repositories',
+//     res: (req) => {
+//       const language = req.url.searchParams.get('q').split('language:')[1];
+//       return {
+//         items: [
+//           { id: 1, full_name: `${language}_one` },
+//           { id: 2, full_name: `${language}_two` },
+//         ],
+//       };
+//     },
+//   },
+// ]);
+
+// COPY PASTE
+createServer([
+  {
+    path: '/api/repositories',
+    res: (req) => {
+      const language = req.url.searchParams.get('q').split('language:')[1];
+      return {
         items: [
           { id: 1, full_name: `${language}_one` },
           { id: 2, full_name: `${language}_two` },
         ],
-      })
-    );
-  }),
-];
-
-const server = setupServer(...handlers);
-
-beforeAll(() => {
-  server.listen();
-});
-
-afterEach(() => {
-  server.resetHandlers();
-});
-
-afterAll(() => {
-  server.close();
-});
+      };
+    },
+  },
+]);
 
 test('renders 2 links for each table(language)', async () => {
   render(
